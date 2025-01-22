973d6498dfac7203c302e7c3cfe1edb051efb2c7
seifahmed322 <seif.ahmed@cic.ae>
2025-01-22 22:05:59 +0200

    Adding Value Help for Company Code

---
+++ app/com.cicre.po/webapp/controller/BaseController.js
@@ -0,0 +1,68 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/ui/core/UIComponent",
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, History, UIComponent, MessageBox, Filter, FilterOperator) {
+	"use strict";
+
+
+	return Controller.extend("com.cicre.po.controller.BaseController", {
+        oldmodelvalue: "",
+		WBSCode: "",
+		WBSCodeGroup: "",
+
+		/**
+		 * Convenience method for accessing the router.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+
+
+		onNavBack: function (oEvent) {
+
+			var oHistory, sPreviousHash;
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("worklist", {}, true /*no history*/);
+			}
+		},
+		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @param {string} [sName] the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel: function (sName) {
+			return this.getView().getModel(sName);
+		},
+
+		/**
+		 * Convenience method for setting the view model.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel: function (oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+
+		/**
+		 * Getter for the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+    })
+});
--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -2,4 +2,12 @@ using my.bookshop as my from '../db/schema';
 
 service CatalogService {
     @readonly entity Books as projection on my.Books;
+    entity CompanyCode @readonly {
+    PERSONID   :  UUID ;
+    LASTNAME : String;
+    FIRSTNAME : String;
+    ADDRESS : String;
+    CITY : String;
+    ID : String;
+  };
 }
